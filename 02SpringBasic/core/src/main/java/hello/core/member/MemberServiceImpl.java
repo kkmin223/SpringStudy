0a93021234c4b5df8cb5debd3d0924bdffa18c29
Kang Min <rkdals133@gmail.com>
2022-01-22 18:13:28 +0900

    스프링 핵심 원리 섹션1~섹션4

---
+++ 02SpringBasic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void Join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
