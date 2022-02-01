5afccc0160f941b6da59a917adc5fe0ff4d597ba
kkmin223 <rkdals133@gmail.com>
2022-02-01 18:45:40 +0900

    iterm 로그인

--- 02SpringBasic/core/src/main/java/hello/core/CoreApplication.java
+++ 02SpringBasic/core/src/main/java/hello/core/CoreApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
+	//완강
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
