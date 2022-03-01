f812eee286eda6a0d891b4a6b6e12c016d9f3b04
Kim Kang Min <rkdals133@gmail.com>
2022-03-01 14:58:45 +0900

    jpa basic 2

---
+++ JPA-Basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String username;
+
+    public Member(){}
+
+    public Member(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
